a8606c7fe1050d883e865d8d7dda30519fed1331
vincent-pli <justdoit.pli@gmail.com>
2015-02-01 23:08:41 +0800

    complete

--- src/main/java/com/pengli/training/answerGenerator/AnswerGenerator.java
+++ src/main/java/com/pengli/training/answerGenerator/AnswerGenerator.java
@@ -1,13 +1,40 @@
 package com.pengli.training.answerGenerator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class AnswerGenerator {
+	private int[] numberRepo = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public int generator() {
+		int[]  exclusiveEmpty = {}, exclusiveZero = { 0 };
+		List<Integer> result = new ArrayList<Integer>();
+		generatorOneFigure(exclusiveZero, result)
+				.generatorOneFigure(exclusiveEmpty, result)
+				.generatorOneFigure(exclusiveEmpty, result)
+				.generatorOneFigure(exclusiveEmpty, result);
+		
+		
+		return result.get(0) * 1000 + result.get(1) * 100 + result.get(2) * 10 + result.get(3) * 1;
+	}
+
+	private AnswerGenerator generatorOneFigure(int[] exclisive, List<Integer> output) {
+		int index = new Random().nextInt(10);
+		int oneFigure = this.numberRepo[index];
+		if (oneFigure != 10 && Arrays.binarySearch(exclisive, oneFigure) < 0) {
+			output.add(oneFigure);
+			this.numberRepo[index] = 10;
+			return this;
+		}
+		generatorOneFigure(exclisive, output);
+
+		return this;
 	}
 
 }
--- src/test/java/com/pengli/training/answerGenerator/AnswerGeneratorTest.java
+++ src/test/java/com/pengli/training/answerGenerator/AnswerGeneratorTest.java
@@ -1,6 +1,42 @@
 package com.pengli.training.answerGenerator;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
 
 public class AnswerGeneratorTest {
-
+	@Test
+	public void shouldbeNumber(){
+		AnswerGenerator answerGenerator = new AnswerGenerator();
+		int number = answerGenerator.generator();
+	}
+	
+	@Test
+	public void shouldnotLessthan_1000(){
+		AnswerGenerator answerGenerator = new AnswerGenerator();
+		int number = answerGenerator.generator();
+		Assert.assertFalse("The number is less than 1000", number < 1000);
+	}
+	
+	@Test
+	public void shouldnotLargethan_9999(){
+		AnswerGenerator answerGenerator = new AnswerGenerator();
+		int number = answerGenerator.generator();
+		Assert.assertFalse("The number is large than 9999", number > 9999);
+	}
+	
+	@Test
+	public void figuresnotRepeat(){
+		AnswerGenerator answerGenerator = new AnswerGenerator();
+		int number = answerGenerator.generator();
+		String numberStr = String.valueOf(number);
+		boolean isRepeat = false;
+		for(int i = 0; i < numberStr.length(); i++){
+			if(numberStr.substring(i+1).indexOf(numberStr.charAt(i)) != -1){
+				isRepeat = true;
+			}
+		}
+		Assert.assertFalse("Figures repeated", isRepeat);
+	}
 }
